9e6d824c112b957ecd2dc21a4ee6d57889f1211b
Werbung06 <Werbung06@skuvgele.local>
2024-05-24 17:28:35 +0200

    add the login form, add some styles

--- kanban-frondend/src/app/app.module.ts
+++ kanban-frondend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './services/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { BoardComponent } from './components/board/board.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -16,9 +22,15 @@ import { BoardComponent } from './components/board/board.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
